e1f7016aeecd87a936557baec2f1307b6ec824bf
vaevas <leixu.xu@formail.com>
2019-07-09 08:34:28 +0800

    格式修改

--- src/components/left.vue
+++ src/components/left.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="col-sm-3 col-md-2 sidebar">
-					<ul class="nav nav-sidebar">
-					<router-link to="/list" tag="li">
-						<a>
-							英雄列表
-						</a>
-					</router-link>
-					<router-link to="/wuqi" tag="li">
-						<a>武器列表</a>
-					</router-link>
-					<router-link to="/zhuangbei" tag="li">
-						<a>装备列表</a>
-					</router-link>
-					<router-link to="/Export" tag="li">
-						<a>Export</a>
-					</router-link>
-					</ul>
-				</div>
+    <ul class="nav nav-sidebar">
+      <router-link to="/list" tag="li">
+        <a>英雄列表</a>
+      </router-link>
+      <router-link to="/wuqi" tag="li">
+        <a>武器列表</a>
+      </router-link>
+      <router-link to="/zhuangbei" tag="li">
+        <a>装备列表</a>
+      </router-link>
+      <router-link to="/Export" tag="li">
+        <a>Export</a>
+      </router-link>
+    </ul>
+  </div>
 </template>
 <script>
-export default {
-	
-}
+export default {};
 </script>
 <style>
-
 </style>
